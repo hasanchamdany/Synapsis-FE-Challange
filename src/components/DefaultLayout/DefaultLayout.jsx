99d003c4066e1cf46e2d96a14d38d8e9ce53246d
hasanchamdany <hasanchamdany@gmail.com>
2023-02-04 18:43:32 +0700

    fix(header): fix header missing value

--- src/components/DefaultLayout/DefaultLayout.jsx
+++ src/components/DefaultLayout/DefaultLayout.jsx
@@ -1,19 +1,22 @@
 // import Footer from 'components/Footer';
-import Navbar from './../Navbar/Navbar';
-import Head from 'next/head';
+import Navbar from "./../Navbar/Navbar";
+import Head from "next/head";
 
-export default function DefaultLayout({ title = 'GMAD 2022', children }) {
-	return (
-		<div className="relative">
-			<Head>
-				<title>{title}</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Navbar />
-			<div className="w-screen max-w-full overflow-x-hidden">
-				<main>{children}</main>
-				{/* <Footer /> */}
-			</div>
-		</div>
-	);
+export default function DefaultLayout({
+  title = "Gorest Public API",
+  children,
+}) {
+  return (
+    <div className="relative">
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <div className="w-screen max-w-full overflow-x-hidden">
+        <main>{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
 }
